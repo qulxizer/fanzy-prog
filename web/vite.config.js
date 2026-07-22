817156d199960b8853f65dc2d21c157f17fe5f81
qulx <qulxizer@gmail.com>
2026-07-22 04:37:51 +0300

    add: webinterface

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,4 +3,9 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
     plugins: [viteSingleFile()],
+    server: {
+        proxy: {
+            '/config': 'http://127.0.0.1:8080',
+        },
+    },
 });
